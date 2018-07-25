bfc23e536e5141ffcc23e3ff41b8fe5dfa720139
Ronny Orbach <ronny@reallygood.co.il>
2018-07-26 00:42:55 +0300

    Merge config and template

--- generate-config.js
+++ generate-config.js
@@ -9,14 +9,16 @@ const GoogleSpreadsheet = require('google-spreadsheet');
 
 const SHEET_ID_TOKEN = '/* #SHEET_ID# */';
 const RULES_TOKEN = '{/* RULES */}';
+const MODULE_START_TOKEN = '/* MODULE START */';;
+const FROM_MODULE_START_TILL_END = /\/\* MODULE START \*\/([\s\S]+)/gm;
 
 // spreadsheet id is the long id in the sheets URL. Pass as an env variable if it's a secret, or put here
 const { SPREADSHEET_ID } = process.env;
 const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 const TEMPLATE_PATH = path.resolve(process.cwd(), '.eslintrc.template.js');
+const HAS_CONFIG = argv.config.length > 0;
 const DEFAULT_OUTPUT_PATH = path.resolve(process.cwd(), 'downloads', '.eslintrc.js');
 const OUTPUT_CONFIG_PATH = argv.config || DEFAULT_OUTPUT_PATH;
-console.log('argv', OUTPUT_CONFIG_PATH)
 
 const onInfo = (err, info) => {
   const [sheet] = info.worksheets;
@@ -46,14 +48,25 @@ const processRows = (err, rows) => {
 
 const prepareConfig = (rules) => {
   const json = JSON.stringify(rules, null, 6);
-  fs.readFile(TEMPLATE_PATH, 'utf8', (err, template) => {
-    let config = template;
+  const template = fs.readFileSync(TEMPLATE_PATH, 'utf8');
+  let config = template
+    .replace(SHEET_ID_TOKEN, SPREADSHEET_ID);
+
+  if (HAS_CONFIG) {
+    const configObject = require(argv.config);
+    configObject.rules = rules;
+    const module = JSON.stringify(configObject, null, 2);
+    const moduleExpression = `module.exports = ${module};`;
+    config = config
+      .replace('Automatically', 'Rules automatically')
+      .replace(FROM_MODULE_START_TILL_END, moduleExpression);
+  } else {
     config = config
       .replace(RULES_TOKEN, json)
-      .replace(SHEET_ID_TOKEN, SPREADSHEET_ID);
+      .replace(MODULE_START_TOKEN, '');
+  }
 
-    writeConfigFile(config);
-  });
+  writeConfigFile(config);
 };
 
 const writeConfigFile = (config) => {
